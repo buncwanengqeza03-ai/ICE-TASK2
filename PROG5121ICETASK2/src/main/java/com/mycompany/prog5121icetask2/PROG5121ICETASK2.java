3511f85b67114b93b49c11029ba0776db30f26ec
buncwanengqeza03-ai <buncwanengqeza03@gmail.com>
2026-09-03 09:05:34 +0200

    first commit

---
+++ PROG5121ICETASK2/src/main/java/com/mycompany/prog5121icetask2/PROG5121ICETASK2.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prog5121icetask2;
+
+/**
+ *
+ * @author Student
+ */
+public class PROG5121ICETASK2 {
+
+    public static void main(String[] args) {
+        String StudentName ="Live"; 
+        String StudentNumber ="ST10155367";
+        int StudentAge = 20;
+        double attendencePercentage = 75.78;
+        char classGroup = 'A';
+        boolean registered = true;
+        final String CAMPUS_NAME = "Rosebank international";
+        
+        System.out.println("Studen name:" + StudentName);
+        System.out.println("Student Number:" + StudentNumber);
+        System.out.println("Student Age:" + StudentAge);
+        System.out.println("Attendence Percentage:" + attendencePercentage);
+        System.out.println("Class Group:" + classGroup);
+        System.out.println("Regtistered:" + registered);
+        System.out.println("Campus Name:" + CAMPUS_NAME);
+        
+    }
+}
